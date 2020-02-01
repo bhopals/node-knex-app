e33d83594c6774d5f07b05b5d909218ac544178d
Bhopal Singh <bsingh@360insights.com>
2020-02-01 18:23:16 -0500

    added react client and use concurrent package to run both servers

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const app = require('express')()
+const bodyParser = require('body-parser')
+
 const ROUTES = require('./route/routes')
 const indexRoute = require('./route/index')
 const detailsRoute = require('./route/details')
 
+// MIDDLEWARE
+app.use(bodyParser.json())
+
+// GET
 app.get(ROUTES.index, indexRoute.get);
 app.get(ROUTES.details, detailsRoute.get);
+
+// POST
+app.post(ROUTES.index, indexRoute.post);
+app.post(ROUTES.details, detailsRoute.post);
+
 
 app.listen('4000');
